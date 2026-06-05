624eb0c4e09b6ed1c99b9ddec230bfb4f7bb265b
burnandy93-rgb <burnandy93@gmail.com>
2026-06-05 00:27:45 -0700

    Add newline and comment in tri9.py

--- tri9.py
+++ tri9.py
@@ -3,3 +3,4 @@ list1 = list(map(int, input("enter first list").split()))
 list2 = list(map(int, input("enter second list").split()))
 common =[num for num in list1 if num in list2]
 print("common elements: ",common)
+#3input lists as follow 10 20 30 40
